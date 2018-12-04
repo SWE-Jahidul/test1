97d7b018c81e6c6e2c47f128342acabf0ac187e1
SWE-Jahidul <programmerjahid162@gmail.com>
2018-12-04 21:17:44 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,32 @@
+
+// for each loop 
+let number = [1,3,4,2]
+ 
+// sort  by element containuously 
+// let sortt = number.sort();
+// console.log(sortt);
+// using pop() method and push method()
+// let a = number.pop();
+// console.log(number)
+
+// let a = number.push(7)
+// console.log(number)
+
+// useing Join() method 
+// console.log(number.join())
+
+
+
+
+// useing toString method
+// console.log(number.toString());
+
+// using for eatch method 
+// function test(num , index)
+// {
+// if(num <= 3 )
+// {
+//     console.log(num + "index : " + index)
+// }
+// }
+// number.forEach(test)
